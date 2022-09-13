fb590a76cd1404a9402e402e0f4ac1d126da8ce4
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2022-09-13 22:43:42 +0100

    error404 done

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../assets/css/Error404.css"
 
 const Error404 = () => {
   return (
     <>
-    <h1 className='text-center'>You be mumu, Ode oshi</h1>
+        <main>
+            <h1>4<span><i className="fa fa-snapchat-ghost"></i></span>4</h1>
+            <h2>Error: 404 page not found</h2>
+            <p>Sorry, the page you're looking for cannot be accessed</p>
+            <p>Go back <Link to="/">Home</Link></p>
+        </main>
     </>
   )
 }
