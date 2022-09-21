2eee2bcbcf8427e4518dcea541749b7b1317b0a5
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2022-09-21 11:48:38 +0100

    Admin in process2

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,11 @@
-// import { useFormik } from 'formik';
+import { useFormik } from 'formik';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import * as yup from "yup";
+import * as yup from "yup";
 import mylogo from "../assets/Screenshot 2022-09-10 140609.png";
 import logoname from "../assets/Screenshot 2022-09-10 141710.png";
+import profilePic from "../assets/blank-profile-picture-973460__340.webp"
+
 
 const Admin = () => {
     const [allUser, setallUser] = useState([]);
@@ -13,6 +15,9 @@ const Admin = () => {
     const [Error, setError] = useState("");
     const [admin, setadmin] = useState([])
     const navigate = useNavigate();
+
+    let firstname = JSON.parse(localStorage.users).firstname
+
 
     const logout = () => {
         localStorage.removeItem("signinEmail")
@@ -41,70 +46,186 @@ const Admin = () => {
         }
     }, []);
 
+    const formik = useFormik({
+        initialValues: {
+            question: "",
+            option1: "",
+            option2: "",
+            option3: "",
+            answer: ""
+        },
+        onSubmit: (values) => {
+            let email = currentuserdetails.email;
+            let user = allUser;
+            let index = user.findIndex((x) => x.email ==email);
+            let remain = parseInt(user[index].account) + Number(1);
+            setallUser((user))
+        },
+
+        onReset: (values) => {
+            console.log(values);
+        },
+
+        validationSchema: yup.object({
+            question: yup.string().required("This field is required"),
+            option1: yup.string().required("This field is required"),
+            option2: yup.string().required("This field is required"),
+            option3: yup.string().required("This field is required"),
+            answer: yup.string().required("This field is required"),
+          }),
+    })
+
   return (
-    <>
-        <nav>
-            <div className="nav-wrapper">
-            <div id='logo'><Link to="/" className="brand-logo"><img style={{borderRadius:"35px"}} src={mylogo} width={42} alt="" /><img src={logoname} alt="" width={70} /></Link></div>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><Link to="/signin" onClick={logout} className='text-danger fw-bold'>Log out</Link></li>
-            </ul>
-            </div>
-        </nav>
+		<>
+			<nav>
+				<div className="nav-wrapper">
+					<div id="logo">
+						<Link to="/" className="brand-logo">
+							<img
+								style={{ borderRadius: "35px" }}
+								src={mylogo}
+								width={42}
+								alt=""
+							/>
+							<img src={logoname} alt="" width={70} />
+						</Link>
+					</div>
+					<ul id="nav-mobile" className="right hide-on-med-and-down">
+						<li className="text-uppercase">
+							<img
+								src={profilePic}
+								width={20}
+								style={{ borderRadius: "20px" }}
+								alt=""
+							/>
+							&nbsp;{firstname}
+						</li>
+					</ul>
+				</div>
+			</nav>
+            <div className='mt-3 col-lg-2 p-2 mx-2 text-center border-3 rounded'>
+				<Link to="/user" className="text-danger fw-bold">⏪&nbsp;Back to dashboard</Link>
+		    </div>
 
-        <div className="container text-light p-5">
-            <div className="row my-2">
-                <h3 className="text-center">SET QUESTIONS</h3>
+			<div className="container text-light p-5">
+				<div className="row my-2">
+					<h3 className="text-center">SET QUESTIONS</h3>
 
-                <div className="row">
-                    <div className="">
-                        <button type='button' data-bs-toggle="modal" data-bs-target="#money" className="btn btn-success bg-success rounded-2">Set Questions</button>
+					<div className="row">
+						<div className="">
+							<center>
+								<h6 className="text-warning mb-5">
+									Click the button below to create your own questions.😊
+								</h6>
+								<button
+									type="button"
+									data-bs-toggle="modal"
+									data-bs-target="#money"
+									className="btn btn-success bg-success rounded-2"
+								>
+									Set Questions
+								</button>
+							</center>
 
-                        <div className="modal bg-success" id='money' data-bs-backdrop="static">
-                            <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <form action="" className='mx-2'>
-                                        <div className="modal-header text-dark">
-                                            <select name="category" >
-                                                <option>Category</option>
-                                                <option value="English">English</option>
-                                                <option value="Biology">Biology</option>
-                                                <option value="Others">Others</option>
-                                            </select>
-                                        </div>
+							<div
+								className="modal bg-success"
+								id="money"
+								data-bs-backdrop="static"
+							>
+								<div className="modal-dialog">
+									<div className="modal-content">
+										<form action="" className="mx-2" onSubmit={formik.handleSubmit}>
+											<div className="modal-body">
+												<label className="col-form-label text-dark">
+													Questions
+												</label>
+												<input
+													type="text"
+													placeholder="Add Question"
+													name="question"
+                                                    onChange={formik.handleChange}
+                                                    onBlur={formik.handleBlur}
+                                                    value={formik.values.question}
+                                                    className={formik.errors.question && formik.touched.question ? "is-invalid form-control" : "form-control"}
+												/>
+                                                {formik.touched.question && <small className='text-danger'>{formik.errors.question}</small>}
+                        
+												<br /><label className="col-form-label text-dark">
+													Options
+												</label>
+												<input
+													type="text"
+													placeholder="Option A"
+													name="option1"
+                                                    onChange={formik.handleChange}
+                                                    onBlur={formik.handleBlur}
+                                                    value={formik.values.option1}
+												/>
+                                                {formik.touched.option1 && <small className='text-danger'>{formik.errors.option1}</small>}
+												<input
+													type="text"
+													placeholder="Option B"
+													name="option2"
+                                                    onChange={formik.handleChange}
+                                                    onBlur={formik.handleBlur}
+                                                    value={formik.values.option2}
+												/>
+                                                {formik.touched.option2 && <small className='text-danger'>{formik.errors.option2}</small>}
+												<input
+													type="text"
+													placeholder="Option C"
+													name="option3"
+                                                    onChange={formik.handleChange}
+                                                    onBlur={formik.handleBlur}
+                                                    value={formik.values.option3}
+												/>
+                                                {formik.touched.option3 && <small className='text-danger'>{formik.errors.option3}</small>}
+												<br /><label className="col-form-label text-dark">
+													Correct Option
+												</label>
+												<input
+													type="text"
+													placeholder="Correct Option"
+													name="answer"
+                                                    onChange={formik.handleChange}
+                                                    onBlur={formik.handleBlur}
+                                                    value={formik.values.answer}
+												/>
+                                                {formik.touched.answer && <small className='text-danger'>{formik.errors.answer}</small>}
+												<br /><button
+													type="submit"
+													className="btn btn-secondary bg-secondary mt-3"
+												>
+													SAVE
+												</button>
+											</div>
+										</form>
 
-                                        <div className="modal-body">
-                                            <label className="col-form-label text-dark">
-                                                Questions
-                                            </label>
-                                            <input type="text" placeholder='Add Question' name='question' />
-                                            <label className="col-form-label text-dark">
-                                                Options
-                                            </label>
-                                            <input type="text" placeholder='Option A' name='option1' />
-                                            <input type="text" placeholder='Option B' name='option2' />
-                                            <input type="text" placeholder='Option C' name='option3' />
-                                            <label className="col-form-label text-dark">
-                                                Correct Option
-                                            </label>
-                                            <input type="text" placeholder='Correct Option' name='answer' />
-                                            <button type='submit' className="btn btn-secondary bg-secondary mt-3">SAVE</button>
-                                        </div>
-                                    </form>
-
-                                    <div className="modal-footer">
-                                        <button type='button' className="btn btn-info bg-info" data-bs-dismiss="modal">Close</button>
-                                        <button type='reset' className="btn btn-danger bg-danger ms-2">Reset</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </>
-  )
+										<div className="modal-footer">
+											<button
+												type="button"
+												className="btn btn-info bg-info"
+												data-bs-dismiss="modal"
+											>
+												Close
+											</button>
+											<button
+												type="reset"
+												className="btn btn-danger bg-danger ms-2"
+                                                onClick={formik.handleReset}
+											>
+												Reset
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Admin
